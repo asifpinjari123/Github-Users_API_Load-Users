1a6aa1721cfa622cb3d7696fc4cdb97483e0f915
asif <asifmansuri736@gmail.com>
2023-08-25 00:47:08 +0530

    save this file

--- src/Components/IonlineComponent/IsOnline.jsx
+++ src/Components/IonlineComponent/IsOnline.jsx
@@ -1,17 +1,17 @@
-import {useEffect} from "react";
-function IsOnline({obj}){
+import { useEffect } from "react";
+function IsOnline({ obj }) {
     console.log("I Am IsOnline Component");
-    let {isOnlineNow,updateIsOnlineOrNot} = obj;
+    let { isOnlineNow, updateIsOnlineOrNot } = obj;
     useEffect(() => {
-        window.addEventListener("online",(e) => {
-            updateIsOnlineOrNot([true,true]);
-            setTimeout(() => updateIsOnlineOrNot([true,false]),5000);
+        window.addEventListener("online", (e) => {
+            updateIsOnlineOrNot([true, true]);
+            setTimeout(() => updateIsOnlineOrNot([true, false]), 5000);
         });
-        window.addEventListener("offline",(e) => {
-            updateIsOnlineOrNot([false,true]);
+        window.addEventListener("offline", (e) => {
+            updateIsOnlineOrNot([false, true]);
         })
-    },[]);
-    return(
+    }, []);
+    return (
         <>
             {isOnlineNow[0] && isOnlineNow[1] && <div className="alert alert-success">Internet Is Connected.</div>}
             {!isOnlineNow[0] && isOnlineNow[1] && <div className="alert alert-warning">You Are Now Offline! Please Check Internet Connection.</div>}
